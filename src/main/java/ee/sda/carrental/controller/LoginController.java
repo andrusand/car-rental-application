dc02ad785ee1c66f55ba89900510c08e73210ea0
Andrus A <75877044+andrusand@users.noreply.github.com>
2021-04-02 17:32:27 +0300

    Quickfix to login page
    Fixed login controller and login html page.

--- src/main/java/ee/sda/carrental/controller/LoginController.java
+++ src/main/java/ee/sda/carrental/controller/LoginController.java
@@ -1,4 +1,4 @@
-package ee.sda.carrental.controllers;
+package ee.sda.carrental.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -6,6 +6,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,7 +17,9 @@ public class LoginController {
 
 
     @GetMapping("/login")
-    String login() {return "login";}
+    String login(){
+        return "login";
+    }
 
     @GetMapping("/logout")
     String logout (HttpServletRequest req, HttpServletResponse res){
