34a5b9b2c67a2bf09ce69ca29b77cceac272e0b8
Andrus A <75877044+andrusand@users.noreply.github.com>
2021-04-07 21:24:56 +0300

    FR003-feature-car-listing
    Added company logo and fixed Company and Branch naming in thymeleaf for car listings.

--- src/main/java/ee/sda/carrental/repository/CarRepository.java
+++ src/main/java/ee/sda/carrental/repository/CarRepository.java
@@ -1,11 +1,13 @@
 package ee.sda.carrental.repository;
 
 import ee.sda.carrental.entity.Car;
+import ee.sda.carrental.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CarRepository extends JpaRepository<Car, Integer> {
-
 
 }
